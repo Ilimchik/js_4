6734d7e23142d317ac552942f94c4d91d8e3266f
Ilimchik <ilimbork@gmail.com>
2023-11-29 14:18:47 +0600

    first commit

---
+++ core.js
@@ -0,0 +1,17 @@
+let agayName = "Almaz"
+let myLocation = "Far from home"
+let myNotebookName = "acer"
+let ourTodayLanguage = "Java Script"
+let myName = "ILim"
+
+let myage = 16
+let todayDate = 29
+let nowTime = 14.05
+let inClassPeople = 20
+let myBirthday
+
+let iam16 = true
+let iHaveOneMilionDollar = false
+let imSmart = true
+let imStupid = true
+let ardakSmart = false
